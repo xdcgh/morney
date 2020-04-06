2899ce61a8d7ce2c1573d8af59ec9d710e3881c2
xdc <1434412784@qq.com>
2020-04-06 20:20:06 +0800

    Mnoey 的 HTML 和 CSS 基本写好

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -27,7 +27,7 @@
   @import "~@/assets/style/helper.scss";
 
   nav {
-    box-shadow: 0 0 3px rgba(0,0,0,0.25);
+    @extend %outerShadow;
     flex-direction: row;
     display: flex;
     font-size: 12px;
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,22 +1,23 @@
 <template>
-  <Layout>
-    <div class="tags">
-      <ul class="current">
-        <li>衣</li>
-        <li>食</li>
-        <li>住</li>
-        <li>行</li>
-      </ul>
-      <div class="new">
-        <button>新增标签</button>
+  <Layout class-prefix="layout">
+    <div class="numberPad">
+      <div class="output">100</div>
+      <div class="buttons">
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>删除</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>清空</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button class="ok">OK</button>
+        <button class="zero">0</button>
+        <button>.</button>
       </div>
-    </div>
-
-    <div>
-      <label class="notes">
-        <span class="name">备注</span>
-        <input type="text" placeholder="请在这里输入备注">
-      </label>
     </div>
 
     <div>
@@ -26,29 +27,23 @@
       </ul>
     </div>
 
-    <div class="numberPad">
-      <div class="output">100</div>
-      <div class="buttons">
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>+</button>
-        <button>删除</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>-</button>
-        <button>清空</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>×</button>
-        <button>OK</button>
-        <button>0</button>
-        <button>.</button>
-        <button>%</button>
-        <button>÷</button>
+    <div>
+      <label class="notes">
+        <span class="name">备注</span>
+        <input type="text" placeholder="请在这里输入备注">
+      </label>
+    </div>
+
+    <div class="tags">
+      <div class="new">
+        <button>新增标签</button>
       </div>
+      <ul class="current">
+        <li>衣</li>
+        <li>食</li>
+        <li>住</li>
+        <li>行</li>
+      </ul>
     </div>
   </Layout>
 </template>
@@ -59,15 +54,26 @@
   };
 </script>
 
+<style lang="scss">
+  .layout-content{
+    display: flex;
+    flex-direction: column-reverse;
+  }
+</style>
+
 <style lang="scss" scoped>
   @import "~@/assets/style/helper.scss";
 
   .tags {
     font-size: 14px;
     padding: 16px;
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column-reverse;
 
     > .current {
       display: flex;
+      flex-wrap: wrap;
 
       > li {
         background: #d9d9d9;
@@ -77,6 +83,7 @@
         border-radius: $h/2;
         padding: 0 16px;
         margin-right: 12px;
+        margin-top: 4px;
       }
     }
 
@@ -127,7 +134,7 @@
       align-items: center;
       position: relative;
 
-      &.selected::after{
+      &.selected::after {
         content: '';
         position: absolute;
         bottom: 0;
@@ -135,6 +142,70 @@
         width: 100%;
         height: 4px;
         background: #333333;
+      }
+    }
+  }
+
+  .numberPad {
+
+    .output {
+      @extend %clearFix;
+      @extend %innerShadow;
+
+      font-size: 36px;
+      /* 等宽字体 */
+      font-family: Consolas, monospace;
+      padding: 9px 16px;
+      text-align: right;
+    }
+
+    .buttons {
+      @extend %clearFix;
+
+      > button {
+        width: 25%;
+        height: 64px;
+        background: transparent;
+        border: none;
+
+        &.ok {
+          height: 64*2px;
+          float: right;
+        }
+
+        &.zero {
+          width: 25*2%;
+        }
+
+        $bg: #f2f2f2;
+
+        &:nth-child(1) {
+          background: $bg;
+        }
+
+        &:nth-child(2), &:nth-child(5) {
+          background: darken($bg, 4%);
+        }
+
+        &:nth-child(3), &:nth-child(6), &:nth-child(9) {
+          background: darken($bg, 4*2%);
+        }
+
+        &:nth-child(4), &:nth-child(7), &:nth-child(10) {
+          background: darken($bg, 4*3%);
+        }
+
+        &:nth-child(8), &:nth-child(11), &:nth-child(13) {
+          background: darken($bg, 4*4%);
+        }
+
+        &:nth-child(14) {
+          background: darken($bg, 4*5%);
+        }
+
+        &:nth-child(12) {
+          background: darken($bg, 4*6%);
+        }
       }
     }
   }
