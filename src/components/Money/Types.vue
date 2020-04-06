3e439ab4708182bde3762f776b86b30775dd2b0c
xdc <1434412784@qq.com>
2020-04-06 21:39:18 +0800

    完成 Types 组件

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -11,25 +11,19 @@
   </div>
 </template>
 
-<script lang="js">
-  export default {
-    name: 'Types',
-    props: ['xxx'],
-    data() {
-      return {
-        type: '-' // '-'表示支出，'+'表示收入
+<script lang="ts">
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class Types extends Vue {
+    type = '-'; // '-'表示支出，'+'表示收入
+
+    selectType(type: string) { // type 只能是 '-' 或是 '+'
+      if (type !== '-' && type !== '+') {
+        throw new Error('type is unknown');
       }
-    },
-    mounted() {
-      console.log(this.xxx)
-    },
-    methods: {
-      selectType(type) { // type 只能是 '-' 或是 '+'
-        if (type !== '-' && type !== '+') {
-          throw new Error('type is unknown')
-        }
-        this.type = type
-      }
+      this.type = type;
     }
   }
 </script>
