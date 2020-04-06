fac632fdde407a4a69824977bdd23a4a03766a6d
xdc <1434412784@qq.com>
2020-04-06 16:33:56 +0800

    完成Money 的 .types 的样式

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -100,16 +100,42 @@
     display: flex;
     align-items: center;
 
-    .name{
+    .name {
       padding-right: 16px;
     }
 
-    input{
+    input {
       height: 64px;
       flex-grow: 1;
       background: transparent;
       border: none;
       padding-right: 16px;
+    }
+  }
+
+  .types {
+    background: #c4c4c4;
+    display: flex;
+    text-align: center;
+    font-size: 24px;
+
+    > li {
+      width: 50%;
+      height: 64px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: relative;
+
+      &.selected::after{
+        content: '';
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 4px;
+        background: #333333;
+      }
     }
   }
 </style>
