0f0bddc9802e728f7c0c9f0566558cee7b6d0334
xdc <1434412784@qq.com>
2020-04-07 17:04:06 +0800

    点击标签，可跳到对应标签的编辑页

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,11 +1,13 @@
 <template>
   <Layout>
-    <ol class="tags">
-      <li v-for="tag in tags" :key="tag.id">
+    <div class="tags">
+      <router-link
+        class="tag" v-for="tag in tags" :key="tag.id"
+        :to="`/labels/edit/${tag.id}`">
         <span>{{tag.name}}</span>
         <Icon name="right"/>
-      </li>
-    </ol>
+      </router-link>
+    </div>
 
     <div class="createTag-wrapper">
       <button class="createTag"
@@ -31,13 +33,13 @@
       const name = window.prompt('请输入标签名');
 
       if (name) {
-          const message = tagListModel.create(name);
+        const message = tagListModel.create(name);
 
-          if (message === 'duplicated') {
-            window.alert('标签名重复了');
-          }else if (message === 'success') {
-            window.alert(name + ', 添加成功！');
-          }
+        if (message === 'duplicated') {
+          window.alert('标签名重复了');
+        } else if (message === 'success') {
+          window.alert(name + ', 添加成功！');
+        }
       }
     }
   }
@@ -49,7 +51,7 @@
     font-size: 16px;
     padding-left: 16px;
 
-    > li {
+    > .tag {
       min-height: 44px;
       display: flex;
       align-items: center;
