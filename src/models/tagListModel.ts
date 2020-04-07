bdc17bb1818f709a2455366cf771c56a56772e1d
xdc <1434412784@qq.com>
2020-04-07 22:12:44 +0800

    解决 id 生成问题

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -1,3 +1,5 @@
+import createId from '@/lib/createId';
+
 const localStorageKeyName = 'tagList';
 
 type Tag = {
@@ -20,13 +22,14 @@ const tagListModel: TagListModel = {
     return this.data;
   },
   create(name) {
+    const id = createId().toString();
     const names = this.data.map(item => item.name);
 
     if (names.indexOf(name) >= 0) {
       return 'duplicated';
     }
 
-    this.data.push({id: name, name: name});
+    this.data.push({id, name});
     this.save();
 
     return 'success';
