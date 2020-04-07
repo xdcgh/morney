c5d957c4cf913f9bd8658780225870b70953415d
xdc <1434412784@qq.com>
2020-04-07 20:52:14 +0800

    添加 EditLabel.vue 的样式，抽离两个组件 Button 和 FormItem

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -10,9 +10,7 @@
     </div>
 
     <div class="createTag-wrapper">
-      <button class="createTag"
-              @click="createTag">新建标签
-      </button>
+      <Button class="createTag" @click.native="createTag">新建标签</Button>
     </div>
   </Layout>
 </template>
@@ -21,11 +19,13 @@
   import Vue from 'vue';
   import {Component} from 'vue-property-decorator';
   import tagListModel from '@/models/tagListModel';
+  import Button from '@/components/Button.vue';
 
   // 初始化数据
   tagListModel.fetch();
-
-  @Component
+  @Component({
+    components: {Button}
+  })
   export default class Labels extends Vue {
     tags = tagListModel.data;
 
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -2,10 +2,10 @@
   <Layout class-prefix="layout">
     <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
     <Types :value.sync="record.type"/>
-    <Notes
-      field-name="备注"
-      placeholder="请在这里输入备注"
-      @update:value="onUpdateNotes"/>
+    <div class="note-wrapper">
+      <FormItem field-name="备注" placeholder="请在这里输入备注"
+                @update:value="onUpdateNotes"/>
+    </div>
     <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
   </Layout>
 </template>
@@ -15,7 +15,7 @@
   import Tags from '@/components/Money/Tags.vue';
   import NumberPad from '@/components/Money/NumberPad.vue';
   import Types from '@/components/Money/Types.vue';
-  import Notes from '@/components/Money/Notes.vue';
+  import FormItem from '@/components/Money/FormItem.vue';
   import {Component, Watch} from 'vue-property-decorator';
   import recordListModel from '@/models/recordListModel';
   import tagListModel from '@/models/tagListModel';
@@ -24,7 +24,7 @@
   const tagList = tagListModel.fetch();
 
   @Component({
-      components: {Tags, Notes, Types, NumberPad}
+      components: {Tags, FormItem, Types, NumberPad}
     }
   )
   export default class Money extends Vue {
@@ -57,5 +57,9 @@
   .layout-content {
     display: flex;
     flex-direction: column-reverse;
+  }
+
+  .note-wrapper {
+    padding: 12px 0;
   }
 </style>
