5e5d8b373fce33bc03a8f023642f1915f916c8f5
xdc <1434412784@qq.com>
2020-04-07 17:00:23 +0800

    新增 EditLabel 组件

--- src/router/index.ts
+++ src/router/index.ts
@@ -21,7 +21,7 @@ const routes: Array<RouteConfig> = [
     path: '/labels',
     component: Labels
   }, {
-    path: '/labels/edit',
+    path: '/labels/edit/:id',
     component: EditLabel
   },
   {
