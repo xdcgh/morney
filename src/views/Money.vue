8ca73c0db63e98be843a40501e7b1c871ab1667c
xdc <1434412784@qq.com>
2020-04-08 23:06:24 +0800

    在 Money.vue 里使用 Tabs，并将通用列表的数据源抽离

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout class-prefix="layout">
     <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
-    <Types :value.sync="record.type"/>
+    <Tabs :data-source="recordTypeList" :value.sync="record.type"/>
     <div class="note-wrapper">
       <FormItem field-name="备注" placeholder="请在这里输入备注"
                 @update:value="onUpdateNotes"/>
@@ -17,12 +17,15 @@
   import Types from '@/components/Money/Types.vue';
   import FormItem from '@/components/Money/FormItem.vue';
   import {Component} from 'vue-property-decorator';
+  import recordTypeList from '@/constants/recordTypeList';
+  import Tabs from '@/components/Tabs.vue';
 
   @Component({
-      components: {Tags, FormItem, Types, NumberPad}
+      components: {Tabs, Tags, FormItem, Types, NumberPad}
     }
   )
   export default class Money extends Vue {
+    recordTypeList = recordTypeList;
     record: RecordItem = {tags: [], notes: '', type: '-', amount: 10};
 
     get recordList() {
