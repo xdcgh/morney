3512a4525c405312ee97226b58499ab49fd83536
xdc <1434412784@qq.com>
2020-04-08 23:08:18 +0800

    删除 Types.vue

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -14,14 +14,13 @@
   import Vue from 'vue';
   import Tags from '@/components/Money/Tags.vue';
   import NumberPad from '@/components/Money/NumberPad.vue';
-  import Types from '@/components/Money/Types.vue';
   import FormItem from '@/components/Money/FormItem.vue';
   import {Component} from 'vue-property-decorator';
   import recordTypeList from '@/constants/recordTypeList';
   import Tabs from '@/components/Tabs.vue';
 
   @Component({
-      components: {Tabs, Tags, FormItem, Types, NumberPad}
+      components: {Tabs, Tags, FormItem, NumberPad}
     }
   )
   export default class Money extends Vue {
