7aa8ab8f4dc20710276d658467661a0994cf56bd
xdc <1434412784@qq.com>
2020-04-08 16:47:16 +0800

    Tags.vue 换用 Vuex ：剩余 4 处 TODO 要处理

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -19,15 +19,16 @@
   @Component({
     computed: {
       tagList() {
-
-        // TODO
-        // return this.$store.fetchTags();
-        return [];
+        return this.$store.state.tagList;
       }
     }
   })
   export default class Tags extends Vue {
     selectedTags: string[] = [];
+
+    created() {
+      this.$store.commit('fetchTags');
+    }
 
     toggle(tag: string) {
       const index = this.selectedTags.indexOf(tag);
@@ -48,8 +49,7 @@
         return window.alert('标签名不能为空');
       }
 
-      // TODO
-      // store.createTag(name);
+      this.$store.commit('createTag', name);
     }
   }
 </script>
