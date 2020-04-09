af74b08fa1d3e55e9e1c088061776f575383a444
xdc <1434412784@qq.com>
2020-04-09 23:29:17 +0800

    保证用户用手机打开页面，否则就提示用户扫码打开

--- src/main.ts
+++ src/main.ts
@@ -24,3 +24,16 @@ window.onload = function () {
     window.scrollTo(0, 10000);
   }, 0);
 };
+
+if (document.documentElement.clientWidth > 500) {
+  window.alert('请使用手机预览本页面，或用控制台开启手机模拟，以保证最佳浏览体验');
+
+  const img = document.createElement('img');
+  img.src = '/qrcode.png';
+  img.style.position = 'fixed';
+  img.style.left = '50%';
+  img.style.top = '50%';
+  img.style.transform = 'translate(-50%,-50%)';
+  img.style.boxShadow = '0 0 10px rgba(0,0,0,0.25)';
+  document.body.appendChild(img);
+}
