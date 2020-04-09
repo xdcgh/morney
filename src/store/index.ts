0e63e5b44fefdf96b5608be950a951f36500ab93
xdc <1434412784@qq.com>
2020-04-09 22:57:07 +0800

    不进行标签的初始化

--- src/store/index.ts
+++ src/store/index.ts
@@ -28,12 +28,6 @@ const store = new Vuex.Store({
     },
     fetchTags(state) {
       state.tagList = JSON.parse(window.localStorage.getItem('tagList') || '[]');
-      if (!state.tagList || state.tagList.length === 0) {
-        store.commit('createTag', '衣');
-        store.commit('createTag', '食');
-        store.commit('createTag', '住');
-        store.commit('createTag', '行');
-      }
     },
     createTag(state, name: string) {
       const id = createId().toString();
